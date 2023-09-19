eacbb203add20d9fa56b9f0e56bdfb1c20b6752e
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-09-19 15:15:48 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   const copyButton = document.getElementById("copyButton");
 
-  copyButton.addEventListener("click", function () {
-    // Write your code here to copy data from the DevTools Network tab.
+  copyButton.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "copyEntities" }, function(response) {
+        if (response && response.success) {
+          alert("Entities copied to clipboard!");
+        }
+      });
+    });
   });
 });
