83a630238b143e0ba87a8cc2aa9c8194c0376112
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-09-19 15:46:12 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
-chrome.webNavigation.onCompleted.addListener(function(details) {
-  // Enable the extension icon when the target site is loaded
-  chrome.action.enable(details.tabId);
-});
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    console.log("Request URL:", details.url);
+    console.log("Request Method:", details.method);
+    console.log("Request Type:", details.type);
+  },
+  { urls: ["<all_urls>"] },
+  ["requestHeaders"]
+);
