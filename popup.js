49173990366f57223033347d58c0eb00ed9b7ac0
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-09-19 14:30:13 -0500

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const copyButton = document.getElementById("copyButton");
+
+  copyButton.addEventListener("click", function () {
+    // Write your code here to copy data from the DevTools Network tab.
+  });
+});
