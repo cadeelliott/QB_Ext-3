21fc7eb9a69d8d109a8093abdaeb3e9d922fb1cd
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-09-19 15:14:41 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,37 +1,4 @@
-// background.js
-
-// Function to fetch data from the "entities" section of the Network tab
-function fetchDataFromEntities() {
-  chrome.devtools.inspectedWindow.eval(
-    
-    // This code runs in the context of the inspected page
-    const entitiesResponse = performance.getEntriesByType('resource')
-      .filter(entry => entry.name.includes('entities.json'))[0];
-    
-    if (entitiesResponse) {
-      fetch(entitiesResponse.name)
-        .then(response => response.json())
-        .then(data => {
-          // Send the data to the extension popup
-          chrome.runtime.sendMessage({ data });
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
-    }
-    ,
-    function (result, isException) {
-      if (isException) {
-        console.error('Error in eval:', result);
-      }
-    }
-  );
-}
-
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === 'copyData') {
-    // Call the function to fetch data and copy it to the clipboard
-    fetchDataFromEntities();
-  }
+chrome.webNavigation.onCompleted.addListener(function(details) {
+  // Enable the extension icon when the target site is loaded
+  chrome.action.enable(details.tabId);
 });
