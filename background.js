83af4221ba14efc657ce9134a60a62d4fbfc7223
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-09-21 11:17:52 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,19 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    console.log("Request URL:", details.url);
-    console.log("Request Method:", details.method);
-    console.log("Request Type:", details.type);
+chrome.webRequest.onCompleted.addListener(
+  (details) => {
+    if (details.method === "GET" && details.url.includes("entities")) {
+      fetch(details.url)
+        .then((response) => response.text())
+        .then((data) => {
+          chrome.scripting.executeScript({
+            target: { tabId: details.tabId },
+            function: (data) => {
+              // Send the data to the content script
+              chrome.runtime.sendMessage({ response: data });
+            },
+            args: [data],
+          });
+        });
+    }
   },
-  { urls: ["<all_urls>"] },
-  ["requestHeaders"]
+  { urls: ["<all_urls>"] }
 );
